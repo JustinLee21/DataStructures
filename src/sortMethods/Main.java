32ea9bd07e862864283b1caeebab44f1a8ec927b
Justin Lemmons <justinlemmons7@gmail.com>
2022-06-06 21:51:05 -0700

    Adding quicksort and merge sort methods

--- src/sortMethods/Main.java
+++ src/sortMethods/Main.java
@@ -7,14 +7,24 @@ public class Main {
 	public static void main(String[] args) {
 		int[] array = {3, 4, 7, 2, 3, 1}; 
 		
+		int[] list1 = {1,2,3,9}; 
+		int[] list2 = {5,6,7,8}; 
+		
 //		BubbleSort.bubbleOrder(array);
 //		System.out.println(Arrays.toString(array));
 		
-		SelectionSort.selection(array);
-		System.out.println(Arrays.toString(array));
+//		SelectionSort.selection(array);
+//		System.out.println(Arrays.toString(array));
 		
 //		InsertionSort.insertOrder(array);
 //		System.out.println(Arrays.toString(array)); 
+		
+		int [] combined = MergeSort.mergeSort(list1, list2); 
+		System.out.println(Arrays.toString(combined)); 
+		
+		int[] quickArray = {6,7,5,2,3,4,8}; 
+		QuickSort.quickSort(quickArray, 0, quickArray.length-1);
+		System.out.println(Arrays.toString(quickArray)); 
 
 
 	}
--- src/sortMethods/MergeSort.java
+++ src/sortMethods/MergeSort.java
@@ -1,5 +1,34 @@
 package sortMethods;
 
 public class MergeSort {
-
+	//Argument int arrays have to be sorted
+	public static int[] mergeSort(int[] arr1, int[] arr2) {
+		int[] newArray = new int [arr1.length + arr2.length]; 
+		int index = 0; 
+		int i = 0; 
+		int j = 0; 
+		while(i < arr1.length && j < arr2.length) {
+			if(arr1[i] < arr2[j]) {
+				newArray[index] = arr1[i]; 
+				index++; 
+				i++; 
+			}else {
+				newArray[index] = arr2[j]; 
+				j++; 
+				index++; 
+			}
+		}
+		while(i < arr1.length) {
+			newArray[index]= arr1[i]; 
+			index++; 
+			i++; 
+		}
+		while(j < arr2.length) {
+			newArray[index]= arr1[j]; 
+			index++; 
+			j++; 
+		}
+		return newArray; 
+	}
+	
 }
